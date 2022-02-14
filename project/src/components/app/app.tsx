d57e447e2632ed0d4cdd244f30b35f8ec5460993
Astemir Maremkulov <amaremkulov96@gmail.com>
2022-02-14 18:20:57 +0300

    Добавляет новые компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,27 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+import Header from '../header/heaer';
+
+type City = {
+  id: number;
+  label: string;
+  isActive: boolean;
+}
+
+type AppPageProps = {
+  offersCount: number;
+  cities: City[];
+}
+
+function App({offersCount, cities}: AppPageProps): JSX.Element {
+  return (
+    <>
+      <Header />
+      <Main
+        offersCount={offersCount}
+        cities={cities}
+      />
+    </>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  OFFERS_COUNT: 5,
+  cities: [
+    {
+      id: 1,
+      label: 'Paris',
+      isActive: false,
+    },
+    {
+      id: 2,
+      label: 'Cologne',
+      isActive: false,
+    },
+    {
+      id: 3,
+      label: 'Brussels',
+      isActive: false,
+    },
+    {
+      id: 4,
+      label: 'Amsterdam',
+      isActive: true,
+    },
+    {
+      id: 5,
+      label: 'Hamburg',
+      isActive: false,
+    },
+    {
+      id: 6,
+      label: 'Dusseldorf',
+      isActive: false,
+    },
+  ],
+
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      offersCount={Setting.OFFERS_COUNT}
+      cities={Setting.cities}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
