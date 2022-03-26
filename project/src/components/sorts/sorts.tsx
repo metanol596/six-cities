d9e24242b8bbd391d4fde7da1b9a3815fd220afe
Astemir Maremkulov <amaremkulov96@gmail.com>
2022-03-27 00:13:53 +0300

    Исправления

--- project/src/components/sorts/sorts.tsx
+++ project/src/components/sorts/sorts.tsx
@@ -16,7 +16,7 @@ function Sorts(): JSX.Element {
 
   return (
     <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-caption">Sort by</span>{' '}
       <span className="places__sorting-type" onClick={toggleSortBlock} tabIndex={0}>
         {sortType}{' '}
         <svg className="places__sorting-arrow" width="7" height="4">
--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -1,16 +1,18 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
+import cn from 'classnames';
 
 import Header from '../../components/header/header';
 //import Spinner from '../../components/spinner/spinner';
 
 import { CITIES } from '../../const';
 
-import { AuthData } from '../../types/auth-data';
+import { getRandomArrayElement } from '../../utils';
 
 import { useAppDispatch } from '../../hooks';
 
 import { loginAction } from '../../store/api-actions';
+import { cityChange } from '../../store/action';
 
 
 import styles from './login.module.css';
@@ -31,16 +33,13 @@ const authFormFields = {
   password: 'Password',
 };
 
-const randomCityIndex = Math.floor(Math.random() * CITIES.length);
-const randomCity = CITIES[randomCityIndex];
+const randomCity = getRandomArrayElement(CITIES);
+
+const EMAIL_VALID_SYMBOLS = /[-.\w]+@([\w-]+\.)+[\w-]+/g;
+const PASSWORD_VALID_SYMBOLS = /([a-z]+[0-9])|([0-9]+[a-z])/gi;
 
 function Login():JSX.Element {
-  const EMAIL_VALID_SYMBOLS = /[-.\w]+@([\w-]+\.)+[\w-]+/g;
-  const PASSWORD_VALID_SYMBOLS = /\w/;
-
   const dispatch = useAppDispatch();
-
-  const isValidField = (value: string, symbols: RegExp) => value !== '' && (value.match(symbols));
 
   const [formState, setFormState] = useState<FormStateProps>({
     email: {
@@ -57,17 +56,17 @@ function Login():JSX.Element {
     },
   });
 
-  const onSubmit = (authData: AuthData) => {
-    dispatch(loginAction(authData));
-  };
+  const isValidField = (value: string, symbols: RegExp) => value !== '' && (value.match(symbols));
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
-    onSubmit({
+    const authData = {
       login: formState.email.value,
       password: formState.password.value,
-    });
+    };
+
+    dispatch(loginAction(authData));
   };
 
   const handleChange = ({target}: ChangeEvent<HTMLInputElement>) => {
@@ -103,8 +102,9 @@ function Login():JSX.Element {
               onSubmit={handleSubmit}
             >
               {Object.entries(authFormFields).map(([name, label]) => {
-                const isInputError = formState[name].error && styles['form__input--error'];
-                const inputClass = `login__input form__input ${isInputError}`;
+                const inputClass = cn('login__input', 'form__input', {
+                  [styles['form__input--error']]: formState[name].error,
+                });
 
                 return (
                   <div
@@ -148,6 +148,7 @@ function Login():JSX.Element {
               <Link
                 to="/"
                 className="locations__item-link"
+                onClick={() => {dispatch(cityChange(randomCity));}}
               >
                 <span>{randomCity}</span>
               </Link>
